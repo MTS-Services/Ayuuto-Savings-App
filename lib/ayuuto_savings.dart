b9208263a93207d1b165e6c3ee72b20bf795f9c6
mdtayobali13 <mdtayobalideveloper@gmail.com>
2025-04-27 17:57:10 +0600

    Revert "Jabed"

--- lib/ayuuto_savings.dart
+++ lib/ayuuto_savings.dart
@@ -1,5 +1,4 @@
-import 'package:ayuuto_savings_app/navigation_menu.dart';
-import 'package:ayuuto_savings_app/src/view/screen/Admin%20Home%20Screen/admin_home_screens.dart';
+import 'package:ayuuto_savings_app/src/view/screen/auth/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -12,7 +11,7 @@ class AyuutoSavings extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NavigationMenu(),
+      home: OnboardingScreen(),
       theme: ThemeData(
         scaffoldBackgroundColor: AppColor.themeColor,
         textTheme: _buildTextTheme(),
@@ -38,33 +37,11 @@ class AyuutoSavings extends StatelessWidget {
           ),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: AppColor.borderColor),
+            borderSide: BorderSide(color:AppColor.borderColor) ,
           ),
-
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: AppColor.borderColor),
-          ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: Colors.red),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: AppColor.borderColor),
-          ),
-          contentPadding: EdgeInsets.all(10),
-          hintStyle: TextStyle(
-            color: AppColor.borderColor,
-          ),
-        ),
-        cardTheme: CardTheme(
-          color: AppColor.buttonColor,
-
         contentPadding: EdgeInsets.all(10),
         hintStyle: TextStyle(
           color: AppColor.borderColor,
-
         ),
       errorStyle: TextStyle(
         fontSize: 12
